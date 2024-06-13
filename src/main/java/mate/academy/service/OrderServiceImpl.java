2a5c3fc0e903187ad20bb89a2cd1e53515670d59
Hrabenko Vadim <grabenkovadim11@gmail.com>
2024-06-13 23:19:27 +0300

    Begin

---
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -0,0 +1,31 @@
+package mate.academy.service;
+
+import mate.academy.model.Order;
+import mate.academy.model.Product;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+    private static final Logger logger = LogManager.getLogger(OrderServiceImpl.class);
+
+    @Override
+    public Order completeOrder(Long userId) {
+        logger.info("Method completeOrder was called with params: userId {}", userId);
+        List<Product> products = getAllProductsFromShoppingCart(userId);
+        Order order = new Order(products, userId);
+        order.setOrderId(1L);
+        return order;
+    }
+
+    private List<Product> getAllProductsFromShoppingCart(Long userId) {
+        Product iphone = new Product("iPhone X", BigDecimal.valueOf(1199));
+        Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
+        Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
+        List<Product> products = List.of(iphone, macBook, xiaomi);
+        logger.info("Data was successfully fetched from DB. params: userId {}", userId);
+        return products;
+    }
+}
